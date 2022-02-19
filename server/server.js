bdb921830e4b906dec61719284b6d4bd073bd0f4
Elisey Budnitsky <bgs6544@gmail.com>
2022-02-19 09:47:38 +0300

    feat: add root get response

---
+++ server/server.js
@@ -0,0 +1,38 @@
+var express = require("express")
+var app = express()
+
+var formidable = require("express-formidable")
+app.use(formidable())
+
+var mongodb = require("mongodb")
+var mongoClient = mongodb.MongoClient
+var objectId = mongodb.ObjectID
+
+var http = require("http").createServer(app)
+var bcrypt = require("bcrypt")
+var fileSystem = require("fs")
+
+var jwt = require("jsonwebtoken")
+var accessTokenSecret = "MyAccessTokenSecret1234567890"
+
+app.use("/public", express.static(__dirname + "/public"))
+app.set("view engine", "ejs")
+
+var socketIO = require("socket.io")(http)
+var socketID = ""
+var users = []
+
+var mainURL = "http://localhost:3000"
+
+socketIO.on("connection", function (socket) {
+    console.log("user connected", socket.id)
+    socketID = socket.id
+})
+
+http.listen(3000, function () {
+    console.log("server started")
+    app.get("/", function (request, result) {
+        result.send("hell")
+    })
+})
+
