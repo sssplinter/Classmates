ec697593d6b01cdbf0598d3e291c11032fffc41b
Elisey Budnitsky <bgs6544@gmail.com>
2022-03-04 12:52:40 +0300

    feat: add checking for authorization in get all users route

--- server/routes/allUsersRoute.js
+++ server/routes/allUsersRoute.js
@@ -2,11 +2,36 @@ module.exports = allUsersRoute
 
 function allUsersRoute(app, database) {
     app.get("/getAllUsers", function (request, result) {
-        database.collection("users").find().toArray(function (error, data) {
-            result.json({
-                "status": "success",
-                "message": "All users",
-                "data": data
+        const accessToken = request.headers.access;
+        database.collection("users").findOne({
+            "accessToken": accessToken
+        }, function (error, user) {
+            database.collection("users").find().toArray(function (error, data) {
+                const modifiedData = data.map(userData => {
+                    let data;
+                    if (user == null) {
+                        data = {
+                            "email": userData.email,
+                            "name": userData.name,
+                            "surname": userData.surname,
+                            "patronim": userData.patronim
+                        }
+                    } else {
+                        data = {
+                            "email": userData.email,
+                            "name": userData.name,
+                            "surname": userData.surname,
+                            "patronim": userData.patronim,
+                            "education": userData.education
+                        }
+                    }
+                    return data;
+                })
+                result.json({
+                    "status": "success",
+                    "message": "All users",
+                    "data": modifiedData
+                });
             });
         });
     });
