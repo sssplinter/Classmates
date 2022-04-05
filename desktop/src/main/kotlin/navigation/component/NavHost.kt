b4c079028e6b66e54f9af95dd5dfc7e970b2003c
Elisey Budnitsky <bgs6544@gmail.com>
2022-04-05 18:43:34 +0300

    refactor: remove useless code

--- desktop/src/main/kotlin/navigation/component/NavHost.kt
+++ desktop/src/main/kotlin/navigation/component/NavHost.kt
@@ -17,7 +17,6 @@ fun NavHost(
 private fun NavHost(navController: NavHostController) {
     var currentScreen by remember { mutableStateOf(navController.currentRoute) }
     navController.setOnNavigationChange { route ->
-        currentScreen = ""
         currentScreen = route
     }
     val screen = navController.graphBuilder.getScreen(currentScreen)
