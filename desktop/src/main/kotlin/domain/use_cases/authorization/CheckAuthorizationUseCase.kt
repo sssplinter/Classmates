7545ce12ae0e71a7bad91263e0c5901162aa9f99
Elisey Budnitsky <bgs6544@gmail.com>
2022-04-09 09:59:17 +0300

    refactor: change use case directory

--- desktop/src/main/kotlin/domain/use_cases/authorization/CheckAuthorizationUseCase.kt
+++ desktop/src/main/kotlin/domain/use_cases/authorization/CheckAuthorizationUseCase.kt
@@ -1,4 +1,4 @@
-package domain.use_cases
+package domain.use_cases.authorization
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -7,8 +7,10 @@ import presentation.screens.splash_screen.SplashScreenViewModel
 
 class CheckAuthorizationUseCase {
     var isFirstTime = true
+
     suspend operator fun invoke(): SplashScreenViewModel.AuthResult = withContext(Dispatchers.IO) {
         delay(2000)
+
         if (isFirstTime) {
             isFirstTime = false
             return@withContext SplashScreenViewModel.AuthResult.Failed(300)
