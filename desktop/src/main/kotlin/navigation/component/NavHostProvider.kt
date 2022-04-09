f6598319518f39b15a071f78e21731ffb09c18c7
Elisey Budnitsky <bgs6544@gmail.com>
2022-04-09 10:55:19 +0300

    fix: make nav host provider as factory

--- desktop/src/main/kotlin/navigation/component/NavHostProvider.kt
+++ desktop/src/main/kotlin/navigation/component/NavHostProvider.kt
@@ -2,12 +2,5 @@ package navigation.component
 
 import androidx.compose.runtime.Composable
 
-private var navController: NavHostController? = null
-
 @Composable
-fun rememberNavController(): NavHostController {
-    if (navController == null) {
-        navController = NavHostController()
-    }
-    return navController!!
-}
+fun rememberNavController() = NavHostController()
