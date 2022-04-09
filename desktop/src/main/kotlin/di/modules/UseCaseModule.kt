ac9350c413c417b53ae0869cc984bcf49dbf1fb8
Elisey Budnitsky <bgs6544@gmail.com>
2022-04-09 10:00:50 +0300

    feat: add sign in and sign up use cases

--- desktop/src/main/kotlin/di/modules/UseCaseModule.kt
+++ desktop/src/main/kotlin/di/modules/UseCaseModule.kt
@@ -1,10 +1,14 @@
 package di.modules
 
-import domain.use_cases.CheckAuthorizationUseCase
+import domain.use_cases.authorization.CheckAuthorizationUseCase
+import domain.use_cases.authorization.SignInUseCase
+import domain.use_cases.authorization.SignUpUseCase
 import org.kodein.di.DI
 import org.kodein.di.bind
 import org.kodein.di.singleton
 
 val useCaseModule = DI.Module(name = "useCase", allowSilentOverride = false) {
     bind { singleton { CheckAuthorizationUseCase() } }
+    bind { singleton { SignInUseCase() } }
+    bind { singleton { SignUpUseCase() } }
 }
---
+++ desktop/src/main/kotlin/domain/use_cases/authorization/SignUpUseCase.kt
@@ -0,0 +1,22 @@
+package domain.use_cases.authorization
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import presentation.screens.login_screen.LoginScreenViewModel
+
+class SignUpUseCase {
+    var isFirstTime = true
+
+    suspend operator fun invoke(email: String, password: String):
+            LoginScreenViewModel.AuthResult = withContext(Dispatchers.IO) {
+        delay(2000)
+
+        if (isFirstTime) {
+            isFirstTime = false
+            return@withContext LoginScreenViewModel.AuthResult.Failed(300)
+        }
+
+        return@withContext LoginScreenViewModel.AuthResult.UnAuthorized
+    }
+}
