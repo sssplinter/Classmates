a2a7e9360b758034c53796bc1c2fdea4c35dffe3
Elisey Budnitsky <bgs6544@gmail.com>
2022-04-12 18:33:02 +0300

    fix: change parent container for main screen

--- desktop/src/main/kotlin/presentation/screens/main_screen/MainScreen.kt
+++ desktop/src/main/kotlin/presentation/screens/main_screen/MainScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -30,75 +31,82 @@ fun MainScreen(navController: NavHostController) {
     var currentTabRoute by remember { mutableStateOf(Screen.Messages.route) }
     Box(
         modifier = Modifier
-            .padding(SMALL_PADDING)
-            .fillMaxSize()
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
-        Row(modifier = Modifier.fillMaxSize()) {
-            Column(
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .clip(MaterialTheme.shapes.large)
-                    .background(MaterialTheme.colors.MenuPanel)
-                    .padding(
-                        vertical = SMALL_PADDING,
-                        horizontal = EXTRA_SMALL_PADDING
-                    ),
-                verticalArrangement = Arrangement.SpaceBetween
-            ) {
-                Column {
-                    val buttonsInfo = listOf(
-                        ButtonInfo(Screen.Home.route, Icons.Default.Home),
-                        ButtonInfo(Screen.Messages.route, Icons.Default.Email),
-                        ButtonInfo(Screen.Favorite.route, Icons.Default.Star),
-                        ButtonInfo(Screen.People.route, Icons.Default.Person),
-                        ButtonInfo(Screen.Settings.route, Icons.Default.Settings)
-                    )
-                    buttonsInfo.forEach { (route, icon) ->
-                        val buttonBackground: ButtonColors
-                        val tint: Color
-                        if (route == currentTabRoute) {
-                            buttonBackground = ButtonDefaults.textButtonColors(backgroundColor = Color(0xFF6C85F7))
-                            tint = Color.White
-                        } else {
-                            buttonBackground = ButtonDefaults.textButtonColors()
-                            tint = Color(0xFF656F7E)
-                        }
-                        TextButton(
-                            modifier = Modifier.padding(EXTRA_SMALL_PADDING).size(40.dp),
-                            colors = buttonBackground,
-                            onClick = {
-                                currentTabRoute = route
-                                tabNavController.navigate(route)
-                            }
-                        ) {
-                            Icon(
-                                imageVector = icon,
-                                contentDescription = "",
-                                tint = tint
-                            )
-                        }
-                    }
-                }
-                TextButton(
+        Box(
+            modifier = Modifier
+                .padding(SMALL_PADDING)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Row(modifier = Modifier.fillMaxSize()) {
+                Column(
                     modifier = Modifier
-                        .padding(EXTRA_SMALL_PADDING)
-                        .size(40.dp),
-                    shape = MaterialTheme.shapes.large,
-                    colors = ButtonDefaults.textButtonColors(
-                        backgroundColor = MaterialTheme.colors.ProfileOutline
-                    ),
-                    onClick = {
-                        tabNavController.navigate(Screen.Profile.route)
-                    }
+                        .fillMaxHeight()
+                        .clip(MaterialTheme.shapes.large)
+                        .background(MaterialTheme.colors.MenuPanel)
+                        .padding(
+                            vertical = SMALL_PADDING,
+                            horizontal = EXTRA_SMALL_PADDING
+                        ),
+                    verticalArrangement = Arrangement.SpaceBetween
                 ) {
-                    Image(
-                        painter = painterResource("no_user_photo.svg"),
-                        contentDescription = ""
-                    )
+                    Column {
+                        val buttonsInfo = listOf(
+                            ButtonInfo(Screen.Home.route, Icons.Default.Home),
+                            ButtonInfo(Screen.Messages.route, Icons.Default.Email),
+                            ButtonInfo(Screen.Favorite.route, Icons.Default.Star),
+                            ButtonInfo(Screen.People.route, Icons.Default.Person),
+                            ButtonInfo(Screen.Settings.route, Icons.Default.Settings)
+                        )
+                        buttonsInfo.forEach { (route, icon) ->
+                            val buttonBackground: ButtonColors
+                            val tint: Color
+                            if (route == currentTabRoute) {
+                                buttonBackground = ButtonDefaults.textButtonColors(backgroundColor = Color(0xFF6C85F7))
+                                tint = Color.White
+                            } else {
+                                buttonBackground = ButtonDefaults.textButtonColors()
+                                tint = Color(0xFF656F7E)
+                            }
+                            TextButton(
+                                modifier = Modifier.padding(EXTRA_SMALL_PADDING).size(40.dp),
+                                colors = buttonBackground,
+                                onClick = {
+                                    currentTabRoute = route
+                                    tabNavController.navigate(route)
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = icon,
+                                    contentDescription = "",
+                                    tint = tint
+                                )
+                            }
+                        }
+                    }
+                    TextButton(
+                        modifier = Modifier
+                            .padding(EXTRA_SMALL_PADDING)
+                            .size(40.dp),
+                        shape = MaterialTheme.shapes.large,
+                        colors = ButtonDefaults.textButtonColors(
+                            backgroundColor = MaterialTheme.colors.ProfileOutline
+                        ),
+                        onClick = {
+                            tabNavController.navigate(Screen.Profile.route)
+                        }
+                    ) {
+                        Image(
+                            painter = painterResource("no_user_photo.svg"),
+                            contentDescription = ""
+                        )
+                    }
                 }
-            }
-            Box(modifier = Modifier.padding(start = SMALL_PADDING)) {
-                HomeNavGraph(tabNavController)
+                Box(modifier = Modifier.padding(start = SMALL_PADDING)) {
+                    HomeNavGraph(tabNavController)
+                }
             }
         }
     }
