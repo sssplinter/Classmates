b804007bc5e7b136336d71709e923bd38d83b231
Elisey Budnitsky <bgs6544@gmail.com>
2022-04-24 04:13:56 +0300

    feat: add current user data

---
+++ desktop/src/main/kotlin/domain/entities/data/CurrentUser.kt
@@ -0,0 +1,11 @@
+package domain.entities.data
+
+object CurrentUser {
+    val token: String = ""
+    val name: String = ""
+    val surname: String = ""
+    val email: String = ""
+    val bio: String = ""
+    val profileImageUrl: String = ""
+    val university: List<Any> = emptyList()
+}
