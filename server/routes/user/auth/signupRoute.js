a6bbb2eaf8f79f62c6ba7fe8e089697c99e25bbb
Elisey Budnitsky <bgs6544@gmail.com>
2022-04-24 07:57:54 +0300

    fix: add checks for sign in and sign up requests

--- server/routes/user/auth/signupRoute.js
+++ server/routes/user/auth/signupRoute.js
@@ -12,40 +12,49 @@ function signupRoute(app, database) {
         const email = request.fields.email;
         const password = request.fields.password;
 
-        database.collection("users").findOne({
-            "email": email
-        }, function (error, user) {
-            if (user == null) {
-                bcrypt.hash(password, salt, function (error, hash) {
-                    const accessToken = jwt.sign({email: email}, accessTokenSecret);
-                    database.collection("users").insertOne({
-                        _id: ObjectId(),
-                        "email": email,
-                        "password": hash,
-                        "accessToken": accessToken,
-                        "name": "",
-                        "surname": "",
-                        "patronim": "",
-                        "university": [],
-                        "chats": [],
-                        "groupChats": [],
-                        "friends": [],
-                        "subscribers": [],
-                        "subscription": []
-                    }, function (error, data) {
-                        result.json({
-                            "status": "success",
+        if (email == null || password == null) {
+            result.status(400).json({
+                "status": "error",
+                "accessToken": "",
+                "message": "Wrong params"
+            });
+        } else {
+            database.collection("users").findOne({
+                "email": email
+            }, function (error, user) {
+                if (user == null) {
+                    bcrypt.hash(password, salt, function (error, hash) {
+                        const accessToken = jwt.sign({email: email}, accessTokenSecret);
+                        database.collection("users").insertOne({
+                            _id: ObjectId(),
+                            "email": email,
+                            "password": hash,
                             "accessToken": accessToken,
-                            "message": "Signed up successfully"
+                            "name": "",
+                            "surname": "",
+                            "patronim": "",
+                            "university": [],
+                            "chats": [],
+                            "groupChats": [],
+                            "friends": [],
+                            "subscribers": [],
+                            "subscription": []
+                        }, function (error, data) {
+                            result.status(200).json({
+                                "status": "success",
+                                "accessToken": accessToken,
+                                "message": "Signed up successfully"
+                            });
                         });
                     });
-                });
-            } else {
-                result.json({
-                    "status": "error",
-                    "message": "Email or username already exist"
-                });
-            }
-        });
+                } else {
+                    result.status(400).json({
+                        "status": "error",
+                        "accessToken": "",
+                        "message": "Email or username already exist"
+                    });
+                }
+            });
+        }
     });
 }
