37b473dba0407eac252cb8de4d76660eb1d2b44e
Elisey Budnitsky <bgs6544@gmail.com>
2022-04-26 18:57:07 +0300

    fix: change error on not found chat

--- desktop/src/main/kotlin/domain/use_cases/chat/FindChatsUseCase.kt
+++ desktop/src/main/kotlin/domain/use_cases/chat/FindChatsUseCase.kt
@@ -2,13 +2,13 @@ package domain.use_cases.chat
 
 import domain.source.chat.ChatRepository
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.withContext
 
 class FindChatsUseCase(
     private val chatRepository: ChatRepository,
 ) {
     suspend operator fun invoke(text: String = "") = withContext(Dispatchers.IO) {
-        chatRepository.allChats.first().filter { it.name.contains(text, true) }
+        chatRepository.allChats.firstOrNull()?.filter { it.name.contains(text, true) } ?: emptyList()
     }
 }
