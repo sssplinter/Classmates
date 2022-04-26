9bcba6226ec7c75f615dfb7992bc5dc5dbdbef03
Elisey Budnitsky <bgs6544@gmail.com>
2022-04-26 19:19:15 +0300

    feat: add current user class

--- desktop/src/main/kotlin/domain/entities/data/CurrentUser.kt
+++ desktop/src/main/kotlin/domain/entities/data/CurrentUser.kt
@@ -1,11 +1,40 @@
 package domain.entities.data
 
 object CurrentUser {
-    val token: String = ""
-    val name: String = ""
-    val surname: String = ""
-    val email: String = ""
-    val bio: String = ""
-    val profileImageUrl: String = ""
-    val university: List<Any> = emptyList()
+    var userId: String = ""
+        private set
+    var token: String = ""
+        private set
+    var name: String = ""
+        private set
+    var surname: String = ""
+        private set
+    var email: String = ""
+        private set
+    var bio: String = ""
+        private set
+    var profileImageUrl: String = ""
+        private set
+    var universities: List<Any> = emptyList()
+        private set
+
+    fun setData(
+        userId: String = this.userId,
+        token: String = this.token,
+        name: String = this.name,
+        surname: String = this.surname,
+        email: String = this.email,
+        bio: String = this.bio,
+        profileImageUrl: String = this.profileImageUrl,
+        universities: List<Any> = this.universities,
+    ) {
+        this.userId = userId
+        this.token = token
+        this.name = name
+        this.surname = surname
+        this.email = email
+        this.bio = bio
+        this.profileImageUrl = profileImageUrl
+        this.universities = universities
+    }
 }
